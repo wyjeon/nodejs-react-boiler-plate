dfa66b025a0a1901a927d75ebbcbeca2f27c3e0a
wyjeon <wyjeon.info@gmail.com>
2021-02-01 16:41:32 +0900

    #4 MongoDB Model & Schema

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, // 빈칸제거
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    //토큰 유효기간
+    type: Number,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
