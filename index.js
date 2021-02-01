71ce4dc518f9ffd900a8a1a85d8839d6167cab54
wyjeon <wyjeon.info@gmail.com>
2021-02-01 17:08:04 +0900

    #7 BodyParser & PostMan & 회원 가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser"); //req.body
+
+const { User } = require("./models/User"); //유저 모델을 가져온다.
+
+app.use(bodyParser.urlencoded({ extended: true })); //application/x-www-form-urlencode을 분석해서 가져오도록 한다.
+app.use(bodyParser.json()); //application/json 타입을 분석해서 가져오도록한다.
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,6 +24,17 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입할 때 필요한 정보들을 clinet에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  }); // mongoDB Method
 });
 
 app.listen(port, () => {
