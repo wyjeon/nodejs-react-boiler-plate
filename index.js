1a714936abb528f38007e394085fb44526fde423
wyjeon <wyjeon.info@gmail.com>
2021-02-01 16:32:47 +0900

    #3 몽고 DB 연결

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://wyjeon:<password>@cluster0.ofgqd.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connceted..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
