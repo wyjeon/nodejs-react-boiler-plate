88a16ad1657506ee3b0a0d1bdc3c1428d7a1b231
wyjeon <wyjeon.info@gmail.com>
2021-02-02 18:44:58 +0900

    #32 로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 하는데 실패했습니다.");
+      }
+    });
+  };
 
   return (
     <div
@@ -17,6 +27,7 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
