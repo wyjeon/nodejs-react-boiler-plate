ec28ed57941781c617c8b891943f7c4114916820
wyjeon <wyjeon.info@gmail.com>
2021-02-02 15:54:08 +0900

    #21 데이터 Flow & Axios

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.get("/", (req, res) => {
   res.send("Hello World! hi~");
 });
 
+app.get("/api/hello", (req, res) => {
+  res.send("안녕하세요~");
+});
+
 app.post("/api/users/register", (req, res) => {
   //회원 가입할 때 필요한 정보들을 clinet에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
