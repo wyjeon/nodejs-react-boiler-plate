f0ecda38f9f432e955899f7a6b20982b35fffe8b
wyjeon <wyjeon.info@gmail.com>
2021-02-02 18:37:32 +0900

    #31 회원 가입 페이지

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER } from "./types";
 
 export function loginUser(dataTosubmit) {
   const request = axios
@@ -8,6 +8,17 @@ export function loginUser(dataTosubmit) {
 
   return {
     type: LOGIN_USER,
+    payload: request
+  };
+}
+
+export function registerUser(dataTosubmit) {
+  const request = axios
+    .post("/api/users/register", dataTosubmit)
+    .then((response) => response.data);
+
+  return {
+    type: REGISTER_USER,
     payload: request
   };
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
 
-function RegisterPage() {
-  return <div>RegisterPage</div>;
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+    }
+
+    let body = {
+      email: email,
+      password: password,
+      name: name
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to sign up");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh"
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={email} onChange={onEmailHandler} />
+        <label>name</label>
+        <input type="text" value={name} onChange={onNameHandler} />
+        <label>Password</label>
+        <input type="password" value={password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
